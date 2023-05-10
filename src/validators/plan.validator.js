d2e46567a99f3698e42f92d5907f128bc0436a08
JuanEstrella <juan.estrella2098@gmail.com>
2023-05-10 15:46:53 +0200

    fix validator

--- src/validators/plan.validator.js
+++ src/validators/plan.validator.js
@@ -1,21 +1,17 @@
 import { body, param } from "express-validator";
-import { validationHandler } from "../shared/validation.handler";
+import { validatorHandler } from "../shared/validation.handler";
 
 export const findOnePlanValidator = [
   param("id").exists().withMessage("The id parameter is required"),
   (req, res, next) => {
-    validationHandler(req, res, next);
+    console.log(req.params.id);
+    validatorHandler(req, res, next);
   },
 ];
 
-export const createPlanValidator = [
+export const updatePlanValidator = [
   body("nombre").escape().optional(),
-  body("foto")
-    .escape()
-    .optional()
-    .trim()
-    .isURL()
-    .withMessage("Field 'foto' need to be an URL image"),
+  body("foto").optional().trim(),
   body("desc").escape().optional(),
   body("tipoPlan").escape().optional(),
   body("cAutonoma").escape().optional(),
@@ -30,7 +26,6 @@ export const createPlanValidator = [
     .withMessage("Field 'numCoches' needs to be a integer"),
   body("costePlan")
     .escape()
-    .notEmpty()
     .optional()
     .isInt({
       min: 0,
@@ -40,30 +35,28 @@ export const createPlanValidator = [
       "Field 'numCoches' needs to be a integer, min value: 0 and max value: 5"
     ),
   (req, res, next) => {
-    validationHandler(req, res, next);
+    validatorHandler(req, res, next);
   },
 ];
 
 export const deletePlanValidator = [
   param("id").exists().withMessage("The id parameter is required"),
   (req, res, next) => {
-    validationHandler(req, res, next);
+    validatorHandler(req, res, next);
   },
 ];
 
-export const updatePlanValidator = [
+export const createPlanValidator = [
   param("id").exists().withMessage("The id parameter is required"),
   body("nombre")
     .escape()
     .notEmpty()
     .withMessage("Field 'nombre' cannot be empty"),
   body("foto")
-    .escape()
     .notEmpty()
     .withMessage("Field 'foto' cannot be empty")
     .trim()
-    .isURL()
-    .withMessage("Field 'foto' needs to be a URL image"),
+    .isURL(),
   body("desc").escape().notEmpty().withMessage("Field 'desc' cannot be empty"),
   body("tipoPlan")
     .escape()
@@ -100,6 +93,6 @@ export const updatePlanValidator = [
       "Field 'numCoches' needs to be a integer, min value: 0 and max value: 5"
     ),
   (req, res, next) => {
-    validationHandler(req, res, next);
+    validatorHandler(req, res, next);
   },
 ];
